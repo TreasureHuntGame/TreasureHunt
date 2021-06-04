e9558900e1edb4ec63ca16d0cec80aa7610cc641
vitorueno <vitoruenootto@gmail.com>
2021-06-04 15:57:03 -0300

    Fix: instalador - correçao de bug em que o instalador era chamado duas vezes

--- Instalador/instalador.sh
+++ Instalador/instalador.sh
@@ -59,53 +59,6 @@ instalar_pacote() {
 	done
 }
 
-#Inicializa as variavéis de timestamp
-ts=`date +%d-%m-%y_%H:%M:%S`
-tsh=`date`
-
-#Inicializa os logs
-diretorio_log="logs/thi_$ts"
-arquivo_log="$diretorio_log/thi_relatorio.log"
-mkdir -p $diretorio_log
-> $arquivo_log
-
-#Informações gerais sobre o funcionamento do script e do github
-echo "Este script está armazenando os resultados das operações realizadas em um relatório (log)."
-echo "Se você encontrar algum problema, por favor consulte o arquivo: $arquivo_log"
-echo "Se você precisar de ajuda, encontrou um bug, ou quer sugerir uma funcionalidade, crie um 'problema' no github deste projeto!"
-echo -e "\nScript inicializado!\n"
-
-#Se não especificou o arquivo de requisitos, padrao = "requisitos.txt"
-arquivo_req=${1:-"requisitos.txt"}
-
-#Se não for root/sudo
-if [ "$EUID" -ne 0 ]; then
-	finalizar_script "Permissões de super usuário necessárias para instalar os pacotes!"
-fi
-
-#Se o arquivo de requisitos não existir
-if [ ! -f $arquivo_req ]; then
-	finalizar_script "Arquivo de requisitos ($arquivo_req) não foi encontrado!"
-else
-	echo -e "Script inicializado em: $tsh\n" >> $arquivo_log
-	info=$(uname -a)
-	echo -e "Linux: $info\n" >> $arquivo_log
-	info=$(lshw -sanitize) #remove informações sensiveis 
-	echo -e "Hardware:\n$info" >> $arquivo_log
-fi
-
-while IFS="" read -r pacote; do
-	if [ $pacote == "mysql-server" ] && [ ! hash mysql 2>/dev/null]; then # se o mysql não existir na maquina
-		debconf-set-selections <<< 'mysql-server mysql-server/root_password password treasure_hunt' #coloca senha 'padrão', avisar usuário no instalador, e também na 
-		debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password treasure_hunt' # documentação sobre isso, e como mudar.
-		echo "Senha utilizada na instalação do MySQL: treasure_hunt"
-		echo -e "\nPor favor mude a senha do mysql, para isso execute o seguinte comando (super usuário):"
-		echo -e "mysqladmin -u root password SENHA_NOVA\n"
-	fi
-	instalar_pacote $pacote
-done < "$arquivo_req"
-# algum dos gerenciadores acima
-installed_pkgs=() 
 
 #$1 = mensagem de alerta antes de finalizar o script
 finalizar_script(){
