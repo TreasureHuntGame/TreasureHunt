8b3406cd95052f4c0e8e710f11a344de59e8f020
vitorueno <vitoruenootto@gmail.com>
2021-08-03 21:39:12 -0300

    feat(front): melhorado aparencia e funcionamento do 404/403
    adicionado logo e botao de alto contraste; retirado redirecionamento automatico; adiçao de botao para voltar ao index/home; modificado aparencia e estilo padrao do btn-primary

--- TreasureHunt/403.php
+++ TreasureHunt/403.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="TreasureHunt, um Jogo de Caça ao Tesouro de Segurança Computacional">
-    <meta name="keywords" content="TreasureHunt, Treasure Hunt, Segurança Computacional, Cibersegurança, Cybersecurity, Computer Security">
+    <meta name="keywords"
+        content="TreasureHunt, Treasure Hunt, Segurança Computacional, Cibersegurança, Cybersecurity, Computer Security">
     <meta name="author" content="Ricardo de la Rocha Ladeira">
     <title>403 -- TreasureHunt{Security}</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -12,44 +14,58 @@
     <link rel="icon" type="image/png" href="/TreasureHunt/img/favicon.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/TreasureHunt/css/style.css">
-    <script src="/TreasureHunt/js/timeout-0.1.js"></script>
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <![endif]-->
 </head>
+
 <body class="text-light bg-dark">
-    <nav class="navbar navbar-expand-sm navbar-dark justify-content-center">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-collapse collapse justify-content-center" id="collapsibleNavbar">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a>Deu ruim!</a>
+    <input type="checkbox" name="contrast-mode" id="contrast">
+    <div id="page-wrapper">
+        <nav class="navbar navbar-expand-sm navbar-dark justify-content-center">
+            <a class="navbar-brand nav-item" href="home.php">
+                <img src="img/logo.svg" alt="TreasureHunt logo" id="img-logo">
+            </a>
+            <ul class="navbar-nav ml-auto" id="contrast-container" role="presentation">
+                <li>
+                    <label for="contrast" id="contrast-label"
+                        class="form-inline justify-content-end label-link contrast-label" tabindex="0"
+                        data-toggle="tooltip" data-trigger="hover" data-placement="bottom"
+                        title="Recurso de alto contraste" accesskey="a">
+                        <span id="botao-contraste" title="Recurso de alto contraste"></span>
+                        <span class="sr-only">Botão para ativar e desativar recurso de alto contraste</span>
+                    </label>
                 </li>
             </ul>
-        </div>
-    </nav>
-    <main>
-        <div class="jumbotron bg-dark">
-            <h1 class="font-weight-bold" id="erro">
-            TreasureHunt<span id="espaco"> </span><span class="destaque">{</span>403<span class="destaque">}</span>
-            </h1>
-            <p>Você tentou acessar uma página proibida. Volte para o início ou aguarde 10 segundos para o redirecionamento automático <span class="smile destaque">:)</span></p>
-        </div>
-        <div class="form-signin">
-            <button class="btn btn-dark btn-block" type="submit" name="inicio" onclick="voltar()">Voltar para o início</button>
-        </div>
-    </main>
-    <footer class="page-footer font-small">
-        <div class="footer-copyright">
-            <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/" id="creative-commons">
-                <img alt="Licença Creative Commons" src="https://i.creativecommons.org/l/by-nc/4.0/80x15.png">
-            </a>
-            <br>
-            <p>Esta obra está licenciada com uma Licença <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Atribuição-NãoComercial 4.0 Internacional</a>.</p>
-            <p>© 2017-2021</p>
-        </div>
-    </footer>    
+        </nav>
+        <main>
+            <div class="jumbotron bg-dark">
+                <h1 class="font-weight-bold page-title" id="erro">
+                    TreasureHunt<span id="espaco"> </span><span class="destaque chaves-left"
+                        aria-hidden="true"></span>403<span class="destaque chaves-right" aria-hidden="true"></span>
+                </h1>
+                <h2 class="subtitulo">Página Proibida</h2>
+                <p>Você tentou acessar uma página proibida. Volte para o início clicando na logo ou no botão abaixo.
+                    <span class="smile destaque"></span>
+                </p>
+            </div>
+
+            <div class="jumbotron">
+                <a role="button" class="btn btn-primary" href="home.php">Voltar para o ínicio</a>
+            </div>
+        </main>
+        <footer class="page-footer font-small">
+            <div class="footer-copyright">
+                <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/" id="creative-commons"
+                    target="_blank">
+                    <img alt="Licença Creative Commons" src="https://i.creativecommons.org/l/by-nc/4.0/80x15.png">
+                    <br>
+                    <span>Esta obra está licenciada com uma Licença <span lang="en">Creative Commons</span>
+                        Atribuição-NãoComercial 4.0 Internacional</span> (abre em nova janela).</a>
+                <p><span lang="en">©</span> 2017-<?php echo date("Y");?></p>
+            </div>
+        </footer>
+    </div>
 </body>
+
 </html>
