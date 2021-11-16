575e4ee844ea2f84b46bb1cdcae8c5ab61f07358
vitorueno <vitoruenootto@gmail.com>
2021-11-16 17:32:04 -0300

    feat(back): adicionado descriçoes para as imagens do desafio HTML
    adicionado descriçoes para cada imagem do desafio HTML e uma lista com o hash de cada imagem para garantir que a legenda realmente pertence a ela

--- Jogo/Problemas/HTML.sh
+++ Jogo/Problemas/HTML.sh
@@ -16,11 +16,29 @@ fi
 # Sorteia o cabeçalho entre h1 e h6
 TAMANHO_CABECALHO=$(seq 6 | shuf -n 1)
 
+
+ARQUIVOS_BASE="../HTML"
+
 # Sorteia uma imagem
-IMAGEM=$(sort -R ../HTML/imagens.txt | shuf -n 1)
+LINHA_SORTEADA=$(shuf -i 1-$(wc -l <$ARQUIVOS_BASE/imagens.txt) -n 1)
+
+HASH_NO_ARQUIVO=`sed -n ${LINHA_SORTEADA}'p' < $ARQUIVOS_BASE/hash.txt` # retorna a linha do hash respectivo
+IMAGEM=`sed -n ${LINHA_SORTEADA}'p' < $ARQUIVOS_BASE/imagens.txt` # retorna a linha com a url da imagem
+
+HASH_IMAGEM=`curl -s $IMAGEM | sha256sum | head -c 65`
+
+if [ $HASH_NO_ARQUIVO = $HASH_IMAGEM ]
+then LEGENDA=`sed -n ${LINHA_SORTEADA}'p' < $ARQUIVOS_BASE/legendas.txt`
+else LEGENDA="Pichação aleatória sem importância"
+fi 
+
+# shuf -i 1-$(wc -l <ARQUIVO_COM_LINKS_DE_IMGS.txt) -n 1
+# IMAGEM=$(sort -R ../HTML/imagens.txt | shuf -n 1)''
 
 # Coloca a imagem sorteada na tag de imagem no arquivo HTML
 sed -i -e "s~\"\"~\"$IMAGEM\"~g" "$NOVO_ARQUIVO"
+
+sed -i -e "s/Imagem do desafio/$LEGENDA/g" "$NOVO_ARQUIVO"
 
 # Sorteia a cor do parágrafo
 ESTILO=$(seq 999999 | shuf -n 1)
@@ -33,8 +51,19 @@ MINIMO=$(awk '/<body>/{ print NR; }' $NOVO_ARQUIVO)
 MAXIMO=$(awk '/<\/body>/{ print NR; }' $NOVO_ARQUIVO)
 
 # Sorteia uma linha do arquivo para inserir a flag
-LINHA_SORTEADA=$(shuf -i $MINIMO-$MAXIMO -n1)
-LINHA=$(head -n $LINHA_SORTEADA $NOVO_ARQUIVO | tail -1)
+LINHA_SORTEADA_FLAG=$(shuf -i $MINIMO-$MAXIMO -n1)
+LINHA=$(head -n $LINHA_SORTEADA_FLAG $NOVO_ARQUIVO | tail -1)
 
 # Insere a flag na linha selecionada
-sed -i -e "${LINHA_SORTEADA}s~$~ <!-- $FLAG -->~" "$NOVO_ARQUIVO"
+sed -i -e "${LINHA_SORTEADA_FLAG}s~$~ <!-- $FLAG -->~" "$NOVO_ARQUIVO"
+
+
+# se precisar gerar os hashes das imagens novamente
+gerar_hashes() {
+    rm -f hash.txt 
+    touch hash.txt
+    while read line; do 
+        BLA=`curl -s $line | sha256sum | head -c 65`
+        echo $BLA >> hash.txt
+    done < imagens.txt
+}
