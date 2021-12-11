95fc595c46c43bdb8fe5efb750b44bd71a7eaa85
TreasureHuntGame <35384720+TreasureHuntGame@users.noreply.github.com>
2021-12-10 23:59:02 -0300

    refactor(script): atualização do arquivo sol51.sh
    Assim como feito com sol59.sh, o código foi reescrito de forma a não necessitar do arquivo intermediário dados.dat, ficando mais direto.

--- Jogo/Solucoes/sol51.sh
+++ Jogo/Solucoes/sol51.sh
@@ -1,7 +1,5 @@
 NUM_INSTANCIAS=$1
 for i in $(seq $NUM_INSTANCIAS)
 do
-	cat "../$i/$2/saida.out" | base64 -d > "../$i/$2/dados.dat"
-	awk '{ for(i=1;i<=NF;i++) printf("%c",$i); print ""; }' "../$i/$2/dados.dat" | sed 's/.*\(TreasureHunt{.*\)/\1/' | cut -c -20
-	rm "../$i/$2/dados.dat"
+	cat "../$i/$2/saida.out" | base64 -d | awk '{ for(i=1;i<=NF;i++) printf("%c",$i); print ""; }' | sed 's/.*\(TreasureHunt{.*\)/\1/' | cut -c -20
 done
