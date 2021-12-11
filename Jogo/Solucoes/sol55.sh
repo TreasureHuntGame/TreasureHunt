ba08fc62bd5c9e96496ef27bbcbfa40a2234cf44
TreasureHuntGame <35384720+TreasureHuntGame@users.noreply.github.com>
2021-12-11 00:10:39 -0300

    refactor(script): update sol55.sh
    Mesma lógica aplicada na atualização dos arquivos sol59.sh e sol51.sh. Refatoração do código de solução para não utilizar um arquivo intermediário, obtendo assim a solução mais rapidamente.

--- Jogo/Solucoes/sol55.sh
+++ Jogo/Solucoes/sol55.sh
@@ -1,7 +1,5 @@
 NUM_INSTANCIAS=$1
 for i in $(seq $NUM_INSTANCIAS)
 do
-	awk '{ for(i=1;i<=NF;i++) printf("%c",$i); print ""; }' "../$i/$2/saida.out" > "../$i/$2/dados2"
-	awk '{ for(i=1;i<=NF;i++) printf("%c",$i); print ""; }' "../$i/$2/dados2" | sed -n 's/.*\(TreasureHunt\)/\1/p' | cut -c -20
-	rm "../$i/$2/dados2"
+	awk '{ for(i=1;i<=NF;i++) printf("%c",$i); print ""; }' "../$i/$2/saida.out" | awk '{ for(i=1;i<=NF;i++) printf("%c",$i); print ""; }' | sed -n 's/.*\(TreasureHunt\)/\1/p' | cut -c -20
 done
