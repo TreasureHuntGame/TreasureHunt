79ad00c6588cc576eff5bea72bd190351c9c30f2
João Vitor Espig <jotaespig@gmail.com>
2024-11-15 01:38:32 -0300

    fix: erro nome de variavel

--- TreasureHunt/download_desafios.php
+++ TreasureHunt/download_desafios.php
@@ -16,7 +16,7 @@ if (file_exists($arquivo)) {
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
     header('Content-Disposition: attachment; filename="' . $nome_arquivo . '"');
-    header('Content-Length: ' . filesize($file));
+    header('Content-Length: ' . filesize($arquivo));
     header('Cache-Control: must-revalidate');
     header('Pragma: public');
     header('Expires: 0');
